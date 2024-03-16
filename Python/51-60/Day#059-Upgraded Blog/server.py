cbe7770e4b78eb3afaef0ea2b2e88e7c568fcb43
KillerRhys <56098017+KillerRhys@users.noreply.github.com>
2024-03-15 23:44:27 -0400

    Day 60
    Only 40 to go!!

--- Python/51-60/Day#059-Upgraded Blog/server.py
+++ Python/51-60/Day#059-Upgraded Blog/server.py
@@ -4,10 +4,14 @@
     OC:2024.03.14(2055) """
 
 # Imports.
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+import os
 
 posts = requests.get("https://gist.githubusercontent.com/gellowg/389b1e4d6ff8effac71badff67e4d388/raw/fc31e41f8e1a6b713eafb9859f3f7e335939d518/data.json").json()
+USER_EMAIL = os.environ.get('EMAIL')
+USER_PASSWORD = os.environ.get('PASSWORD')
 
 app = Flask(__name__)
 
@@ -22,9 +26,22 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(USER_EMAIL, USER_PASSWORD)
+        connection.sendmail(USER_EMAIL, USER_EMAIL, email_message)
 
 
 @app.route("/post/<int:index>")
